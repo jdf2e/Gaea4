07d95b54d488854025abda8d2a1c27afabf30068
wangnan <wangnan7@jd.com>
2018-11-05 11:18:01 +0800

    update template

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const donwload = require('download-git-repo');
 const ora = require('ora');
 const path = require('path');
 const rm = require('rimraf').sync;
+const Metalsmith = require('metalsmith');
+const replace  = require('metalsmith-text-replace');
+const metalsmithJson = require('./metalsmith.json');
+
 
 program.version(package.version,'-V,--version')
        .option('-U,--update <template-name>','指定更新模板')
@@ -39,7 +43,12 @@ if(program.update){
     }
     next.then(target =>{
 
-        go(target)
+       return go(target)
+    }).then(target=>{
+       
+        generator(target,path.parse(target).dir);
+    }).catch(err=>{
+        console.error(err);
     })
 
 
@@ -63,9 +72,27 @@ function go(targetName){
             }else{
                 spinner.succeed()
                 resolve(target)
+
             }
         })
 
     })
 }
 //处理模板
+
+function generator(src,dest="."){
+    if(!src){
+        console.error(`无效的source地址`);
+    }else{
+        Metalsmith(process.cwd()).clean(false).source(src)
+                  .destination(dest)
+                  .use(replace(metalsmithJson)).build(err => {
+                      if(err){
+                        console.log(logSymbols.error,chalk.red(`模板生成失败，请核对更新的模板是否有问题？`))
+                      }else{
+                        console.log(logSymbols.success,chalk.green(`模板成功生成，请提交git同步`))
+                        rm(src);
+                      }
+                  })
+    }
+}
