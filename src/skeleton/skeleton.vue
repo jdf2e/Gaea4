91d916cf27962b34ae87dfc8fb9f46dfc1576407
wangnan <wangnan7@jd.com>
2019-01-03 18:00:46 +0800

    update skeleton template nutui2.0

--- src/skeleton/skeleton.vue
+++ src/skeleton/skeleton.vue
@@ -1,52 +1,58 @@
 <template>
-    <skeleton-loading>
-        <row>
-            <skeleton-square width="7.5rem" height="5rem" ></skeleton-square>
-        </row>
-        <row padding="0.3rem 0.2rem 0 ">
+    <nut-skeleton>
+        
+        <row padding="15px 10px 0 ">
 
-            <skeleton-square width="4rem"  ></skeleton-square>
-            <skeleton-square width="1rem" margin="0 0 0 0.8rem"></skeleton-square>
-            <skeleton-square width="1rem"  margin="0 0 0 0.2rem"></skeleton-square>
+            <skeleton-square width="200px"  ></skeleton-square>
+            <skeleton-square width="50px" margin="0 0 0 40px"></skeleton-square>
+            <skeleton-square width="50px"  margin="0 0 0 10px"></skeleton-square>
         </row>
-        <row padding="0.4rem 0.2rem">
-            <skeleton-circle  diameter="1rem"></skeleton-circle>
-            <skeleton-square width="5.5rem" :count="3" margin="0.1rem 0.2rem 0.1rem 0.2rem"></skeleton-square>
+        <row padding="20px 10px">
+            <skeleton-circle  diameter="50px"></skeleton-circle>
+            <skeleton-square width="275px" :count="3" margin="5px 10px 5px 10px"></skeleton-square>
         </row>
 
-        <row padding="0 0.2rem 0.4rem">
-            <skeleton-circle  diameter="1rem"></skeleton-circle>
+        <row padding="0 10px 20px">
+            <skeleton-circle  diameter="50px"></skeleton-circle>
             <column>
-                    <skeleton-square width="5.5rem" :count="2" margin="0.1rem 0.2rem 0.1rem 0.2rem"></skeleton-square>
-                    <skeleton-square width="2rem"    margin="0.1rem 0.2rem 0.1rem 0.2rem"></skeleton-square>
+                    <skeleton-square width="275px" :count="2" margin="5px 10px 5px 10px"></skeleton-square>
+                    <skeleton-square width="100px"    margin="5px 10px 5px 10px"></skeleton-square>
             </column>
         </row>
 
-        <row padding="0 0.2rem 0.4rem">
-            <skeleton-square width="1.5rem" height="1.5rem" ></skeleton-square>
-            <column padding="0 0 0 0.2rem">
-                <skeleton-square width="4.5rem"   height="0.2rem" margin="0 0.2rem 0.1rem 0.2rem"></skeleton-square>
-                <skeleton-square width="2rem"  height="0.15rem"   margin="0.1rem 0.2rem 0.1rem 0.2rem"></skeleton-square>
-                <skeleton-square width="4.5rem"  backColor="#ffffff" :count="2"></skeleton-square>
-                <skeleton-square width="1rem"     margin="0 0.2rem 0.1rem 0.2rem"></skeleton-square>
+        <row padding="0 10px 20px">
+            <skeleton-square width="75px" height="75px" ></skeleton-square>
+            <column padding="0 0 0 10px">
+                <skeleton-square width="275px"   height="10px" margin="0 10px 5px 10px"></skeleton-square>
+                <skeleton-square width="100px"  height="7.5px"   margin="5px 10px 5px 10px"></skeleton-square>
+                <skeleton-square width="275px"  backColor="#ffffff" :count="2"></skeleton-square>
+                <skeleton-square width="50px"     margin="0 10px 5px 10px"></skeleton-square>
             </column>
             
         </row>
-            <row padding="0 0.2rem 0.4rem">
-                <skeleton-square width="0.6rem" height="0.6rem" v-for=" i in 5" :key="i" margin="0.3rem"></skeleton-square>
+            <row padding="0 10px 20px">
+                <skeleton-square width="30px" height="30px" v-for=" i in 5" :key="i" margin="15px"></skeleton-square>
             </row>
         
-    </skeleton-loading>
+    </nut-skeleton>
   
 </template>
 
 <script>
-import skeleton from '../component/skeleton/index'
+import {skeleton} from '@nutui/nutui';
+const {nutskeleton,skeletonCircle,skeletonSquare,row,column} = skeleton;
 export default {
     data(){
         return{
 
         }
+    },
+    components:{
+        'nut-skeleton':nutskeleton,
+        'skeleton-square':skeletonSquare,
+        'skeleton-circle':skeletonCircle,
+        'row':row,
+        'column'column
     }
     
 }
