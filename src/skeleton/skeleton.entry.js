6b705ba9cd9a2c0846dde9a73e22862daadd399a
wangnan <wangnan7@jd.com>
2019-01-04 16:53:32 +0800

    update skeleton

--- src/skeleton/skeleton.entry.js
+++ src/skeleton/skeleton.entry.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import skeleton from './skeleton.vue'
+import skeletonComp from '../component/skeleton/index';
+Vue.use(skeletonComp);
 
 export default new Vue({
     components:{
