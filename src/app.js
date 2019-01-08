52362f4b0adcd9342324a75acd71a79d32f757d6
wangnan <wangnan7@jd.com>
2019-01-08 09:37:04 +0800

    udpate template

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './app.vue';
 import router from './router.js';
+
+import { Cell,Dialog } from '@nutui/nutui';
+Vue.use(Cell);
+Vue.use(Dialog);
  new Vue({
   router,
   render: h => h(App),
