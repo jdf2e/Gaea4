41cc9daba164335577763714f40c3ee87de4184a
wangnan <wangnan7@jd.com>
2019-01-14 15:19:18 +0800

    update ts tempalte

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
-
+import { Cell,Dialog } from '@nutui/nutui'
+Vue.use(Cell)
+Vue.use(Dialog)
 
 // 阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
