e29e2ba50be373067c723371b81b5ba2adc84218
lilinsen <lilinsen@jd.com>
2019-10-12 14:10:43 +0800

    删除nutui引用

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
-import { Dialog,Picker } from '@nutui/nutui';
-console.log( Dialog)
-Dialog.install(Vue);
-Picker.install(Vue);
+// import { Dialog } from '@nutui/nutui';
+// Dialog.install(Vue);
+
 // 阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
 export default new Vue({
